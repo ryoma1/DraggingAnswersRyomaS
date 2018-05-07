1d20c18f2e1064db0e42083197c4ffadf1a427ce
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-05-07 10:41:21 -0400

    yes

--- main.lua
+++ main.lua
@@ -14,4 +14,4 @@ local composer = require( "composer" )
 
 -----------------------------------------------------------------------------------------
 -- Go to the intro screen
-composer.gotoScene( "level1_screen" )
+composer.gotoScene( "you_lose" )
