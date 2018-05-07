e4e2f95916790e181d61cae739d80c864f70327a
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-05-07 10:54:48 -0400

    Updated folder

--- main.lua
+++ main.lua
@@ -14,4 +14,4 @@ local composer = require( "composer" )
 
 -----------------------------------------------------------------------------------------
 -- Go to the intro screen
-composer.gotoScene( "you_lose" )
+composer.gotoScene( "level1_screen" )
